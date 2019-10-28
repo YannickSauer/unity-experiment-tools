5ee6cdb0b7ecd3f470762b298a6096cf7f7fdbbe
Yannick <yannick.s204@gmail.com>
2019-10-28 16:34:32 +0100

    Add files via upload

---
+++ ExperimentPreparation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExperimentPreperation : MonoBehaviour
+{
+    public static void ShuffleArray(float[] array) // Fisher–Yates shuffle
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int r =  Random.Range(0, i+1);
+            // swap array[r] and array[i]
+            float temp = array[r];
+            array[r] = array[i];
+            array[i] = temp;
+        }
+    }
+    public static void ShuffleArray(int[] array) // Fisher–Yates shuffle
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int r =  Random.Range(0, i+1);
+            // swap array[r] and array[i]
+            int temp = array[r];
+            array[r] = array[i];
+            array[i] = temp;
+        }
+    }
+}
---
+++ SaveTracking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveTracking : MonoBehaviour
+{
+    public bool saveEyeTracking = true;
+    public bool saveHeadTracking = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO
+    }
+}
