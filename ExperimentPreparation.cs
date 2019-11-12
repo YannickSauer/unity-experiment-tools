ab2bac980562fbf963dc4146c5f493e2aed93ef6
Yannick <yannick.s204@gmail.com>
2019-11-12 16:26:29 +0100

    Updated ExperimentPreparation. SaveTracking is now working.
    Experiment Preparation has now extended functions for permuting arrays.
    SaveTracking can save eye tracking and position as well as orientation of arbitrary game object (e.g. Camera for headset tracking)

--- ExperimentPreparation.cs
+++ ExperimentPreparation.cs
@@ -1,29 +1,164 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExperimentPreperation : MonoBehaviour
+public class ExperimentPreparation
 {
-    public static void ShuffleArray(float[] array) // Fisher–Yates shuffle
+    // randomly permute elements of float array
+    public static void RandPermute(float[] array) // Fisher–Yates shuffle
     {
         for (int i = array.Length - 1; i > 0; i--)
         {
-            int r =  Random.Range(0, i+1);
+            int r = UnityEngine.Random.Range(0, i + 1);
             // swap array[r] and array[i]
             float temp = array[r];
             array[r] = array[i];
             array[i] = temp;
         }
     }
-    public static void ShuffleArray(int[] array) // Fisher–Yates shuffle
+    // randomly permute elements of int array
+    public static void RandPermute(int[] array) // Fisher–Yates shuffle
     {
         for (int i = array.Length - 1; i > 0; i--)
         {
-            int r =  Random.Range(0, i+1);
+            int r = UnityEngine.Random.Range(0, i + 1);
             // swap array[r] and array[i]
             int temp = array[r];
             array[r] = array[i];
             array[i] = temp;
         }
+    }
+
+    // returns a random parmutation of integers from 0 to maxIndex. (random permutation of indices)
+    // This can be used in combination with 'PermuteWithInd' to shuffle muliple arrays with the 
+    // same permutation (for example arrays for different trial conditions that should keep the same sampling)
+    // If you have a condition which alternates between trials, then set alternateCondition to true and the
+    // function will permute even with even indices and odd with odd indices
+    public static int[] RandIndexPermutation(int maxIndex, bool alternateCondition)
+    {
+        int[] indices = new int[maxIndex];
+
+        if (alternateCondition)
+        {
+            int[] tmp_odd;
+            int[] tmp_even;
+            tmp_odd = new int[maxIndex / 2];
+            if (maxIndex % 2 == 0) // if maxIndex is even
+            {
+                 tmp_even = new int[maxIndex / 2];
+            }
+            else
+            {
+                tmp_even = new int[maxIndex / 2 + 1];
+            }
+            
+            for (int i = 0; i < maxIndex / 2; i++)
+            {
+                tmp_even[i] = 2 * i;
+                tmp_odd[i] = 2 * i + 1;
+            }
+            if (maxIndex % 2 == 1) // if maxIndex is odd
+            {
+                 tmp_even[maxIndex / 2 + 1] = maxIndex - 1;
+            }
+            RandPermute(tmp_even);
+            RandPermute(tmp_odd);
+            for (int i = 0; i < maxIndex / 2; i++)
+            {
+                indices[2*i] = tmp_even[i];
+                indices[2*i + 1] = tmp_odd[i];
+            }
+            if (maxIndex % 2 == 1) // if maxIndex is odd
+            {
+                indices[maxIndex - 1] = tmp_even[maxIndex / 2 + 1];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < maxIndex; i++)
+            {
+                indices[i] = i;
+            }
+            RandPermute(indices);
+        }
+        foreach( var i in indices )
+        {
+            Debug.Log( i );
+        }
+        return indices;
+    }
+    public static int[] RandIndexPermutation(int maxIndex)
+    {
+        int[] indices = new int[maxIndex];
+        for (int i = 0; i < maxIndex; i++)
+        {
+            indices[i] = i;
+        }
+        RandPermute(indices);
+        return indices;
+    }
+
+    public static void PermuteWithInd(float[] array, int[] indices)
+    {
+        float[] tmp = new float[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            tmp[i] = array[indices[i]];
+        }
+        Array.Copy(tmp,array,tmp.Length);
+    }
+    public static void PermuteWithInd(int[] array, int[] indices)
+    {
+        int[] tmp = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            tmp[i] = array[indices[i]];
+        }
+        Array.Copy(tmp,array,tmp.Length);
+    }
+
+    // returns an array with the elements of 'fillValues' repeated as given by repElement and repArray:
+    // First repeat each element of 'fillValues' 'repElement' times
+    // Then repeat the result 'repArray' times
+    // Example: 
+    // fillValues = [1 2 3 4]; repElement = 3; repArray = 2;
+    // retuns -> [1 1 1 2 2 2 3 3 3 4 4 4 1 1 1 2 2 2 3 3 3 4 4 4]
+    public static float[] FillWithSamples(float[] fillValues, int repElement, int repArray)
+    {
+        int length = fillValues.Length * repElement * repArray;
+        float[] output = new float[length];
+        int counter = 0;
+        for(int outer = 0; outer < repArray; outer++)
+        {
+            for(int elem = 0; elem < fillValues.Length; elem++)
+            {
+                for(int inner = 0; inner < repElement; inner++)
+                {
+                    output[counter] = fillValues[elem];
+                    counter++;
+                }
+            }
+        }
+        return output;
+    }
+    //integer vesion of the function above
+    public static int[] FillWithSamples(int[] fillValues, int repElement, int repArray)
+    {
+        int length = fillValues.Length * repElement * repArray;
+        int[] output = new int[length];
+        int counter = 0;
+        for(int outer = 0; outer < repArray; outer++)
+        {
+            for(int elem = 0; elem < fillValues.Length; elem++)
+            {
+                for(int inner = 0; inner < repElement; inner++)
+                {
+                    output[counter] = fillValues[elem];
+                    counter++;
+                }
+            }
+        }
+        return output;
     }
 }
--- SaveTracking.cs
+++ SaveTracking.cs
@@ -1,68 +1,159 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using System.Threading;
+using System.Text;
+using ViveSR.anipal.Eye;
 
 public class SaveTracking : MonoBehaviour
 {
-    public bool saveEyeTracking = true;
-    public bool saveHeadTracking = true;
-    // Start is called before the first frame update
+    public enum EyeTrackingOptions
+    {
+        none, left, right, combined, all
+    }
+    [Header("Output Settings")]
+    public string trackingFile = "tracking.csv"; // maybe change to automatically get subject dependend name? // application.getpath
+    public string delimiter = ";";
+    [Header("Eye Tracking Settings")]
+    public EyeTrackingOptions eyeTrackingOptions;
+    [Header("Object Tracking")]
+    public GameObject trackedGameobject;
+    public bool savePosition = true;
+    public bool saveOrientation = true;
+    Queue trackingDataQueue = new Queue();
+    static string msgBuffer = "";
+    VerboseData eyeData;
     void Start()
     {
-        //TODO
+        // TODO: check if csv file already exists and change filename if necessary (e.g. _01...)
+        WriteHeader();
+        InvokeRepeating("Save", 0.0f, 1.0f); // save data to file every second
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //TODO
+        QueueTrackingData();
     }
-    // save array as a column to csv file
-    void SaveArrayToCSV(float[] arrayToSave, string csvPath, string header)
+        public static void Msg(string msg)
     {
-        if (!File.Exists(csvPath))
-        {
-            var outputFile = File.CreateText(csvPath);
-            outputFile.WriteLine(header);
-            for (int i = 0; i < arrayToSave.Length; i++)
-            {
-                outputFile.WriteLine(arrayToSave[i].ToString());
-            }
-            outputFile.Close();
-        }
-        else // append to file a new column
-        {
-            var csv = File.ReadLines(csvPath) // not AllLines
-                .Select((line, index) => index == 0
-                ? line + ";" + header
-                : line + ";" + arrayToSave[index - 1].ToString())
-                .ToList(); // we should write into the same file, that´s why we materialize
-            File.WriteAllLines(csvPath, csv);
-        }
-    }
-    
-    //Same function for int
-    void SaveArrayToCSV(int[] arrayToSave, string csvPath, string header)
-    {
-        if (!File.Exists(csvPath))
-        {
-            var outputFile = File.CreateText(csvPath);
-            outputFile.WriteLine(header);
-            for (int i = 0; i < arrayToSave.Length; i++)
-            {
-                outputFile.WriteLine(arrayToSave[i].ToString());
-            }
-            outputFile.Close();
-        }
-        else // append to file a new column
-        {
-            var csv = File.ReadLines(csvPath) // not AllLines
-                .Select((line, index) => index == 0
-                ? line + ";" + header
-                : line + ";" + arrayToSave[index - 1].ToString())
-                .ToList(); // we should write into the same file, that´s why we materialize
-            File.WriteAllLines(csvPath, csv);
-        }
+        msgBuffer = msg;    
     }
 
+    // TODO Use string builder or string.join
+    // should be much more effiction: https://stackoverflow.com/questions/21078/most-efficient-way-to-concatenate-strings
+    void QueueTrackingData()
+    {   
+        StringBuilder datasetLine = new StringBuilder(300); // adjust capacity to your needs
+        // timestamp: use time at beginning of frame. What makes sense here? Use eye tracker timestamp?
+        datasetLine.Append(Time.time.ToString("F5") + delimiter); // convert with 5 digits after decimal point
+        // eyetracking data
+        switch (eyeTrackingOptions)
+        {
+            case EyeTrackingOptions.left:
+                SRanipal_Eye.GetVerboseData(out eyeData);
+                // write left eye origin
+                datasetLine.Append(eyeData.left.gaze_origin_mm.ToString("F3") + delimiter);
+                // write left eye normalied gaze direction
+                datasetLine.Append(eyeData.left.gaze_direction_normalized.ToString("F3") + delimiter);
+                break;
+            case EyeTrackingOptions.right:
+                SRanipal_Eye.GetVerboseData(out eyeData);
+                // write right eye origin
+                datasetLine.Append(eyeData.right.gaze_origin_mm.ToString("F3") + delimiter);
+                // write right eye normalied gaze direction
+                datasetLine.Append(eyeData.right.gaze_direction_normalized.ToString("F3") + delimiter);
+                break;
+            case EyeTrackingOptions.combined:
+                SRanipal_Eye.GetVerboseData(out eyeData);
+                // write combined eye origin
+                datasetLine.Append(eyeData.combined.eye_data.gaze_origin_mm.ToString("F3") + delimiter);
+                // write combined eye normalied gaze direction
+                datasetLine.Append(eyeData.combined.eye_data.gaze_direction_normalized.ToString("F3") + delimiter);
+                break;
+            case EyeTrackingOptions.all:
+                SRanipal_Eye.GetVerboseData(out eyeData);
+                // write left eye origin
+                datasetLine.Append(eyeData.left.gaze_origin_mm.ToString("F3") + delimiter);
+                // write left eye normalied gaze direction
+                datasetLine.Append(eyeData.left.gaze_direction_normalized.ToString("F3") + delimiter);
+                // write right eye origin
+                datasetLine.Append(eyeData.right.gaze_origin_mm.ToString("F3") + delimiter);
+                // write right eye normalied gaze direction
+                datasetLine.Append(eyeData.right.gaze_direction_normalized.ToString("F3") + delimiter);
+                // write combined eye origin
+                datasetLine.Append(eyeData.combined.eye_data.gaze_origin_mm.ToString("F3") + delimiter);
+                // write combined eye normalied gaze direction
+                datasetLine.Append(eyeData.combined.eye_data.gaze_direction_normalized.ToString("F3") + delimiter);
+                break;
+        }
+        // tracked game object's position
+        if (savePosition)
+            datasetLine.Append(trackedGameobject.transform.position.ToString("F3") + delimiter);
+        // tracked game object's orientation
+        if (saveOrientation)
+            datasetLine.Append(trackedGameobject.transform.rotation.ToString("F3") + delimiter);
+        // buffered message
+        if (!String.IsNullOrEmpty(msgBuffer))
+        {
+            datasetLine.Append(msgBuffer + delimiter);
+            msgBuffer = "";
+        }
+        trackingDataQueue.Enqueue(datasetLine.ToString());
+    }
+
+    void Save()
+    {
+        var thread = new Thread(WriteTrackingData);
+        thread.Start();
+    }
+
+    void WriteTrackingData()
+    {
+        Debug.Log("Writing Data");
+        StreamWriter sw = new StreamWriter(trackingFile, true); //true for append
+        string datasetLine;
+        // dequeue trackingDataQueue until empty
+        while (trackingDataQueue.Count > 0)
+        {
+            datasetLine = trackingDataQueue.Dequeue().ToString();
+            sw.WriteLine(datasetLine); // write to file
+        }
+        sw.Close(); // close file
+        Debug.Log("End Writing Data");
+    }
+    void WriteHeader()
+    {
+        StreamWriter sw = new StreamWriter(trackingFile);
+        string header = "Timestamp" + delimiter;
+        switch (eyeTrackingOptions)
+        {
+            case EyeTrackingOptions.left:
+                header += "(left_eye_origin.x,left_eye_origin.y,left_eye_origin.z)" + delimiter + "(left_eye_gaze.x,left_eye_gaze.y,left_eye_gaze.z)" + delimiter;
+                break;
+            case EyeTrackingOptions.right:
+                header += "(right_eye_origin.x,right_eye_origin.y,right_eye_origin.z)" + delimiter + "(right_eye_gaze.x,right_eye_gaze.y,right_eye_gaze.z)" + delimiter;
+                break;
+            case EyeTrackingOptions.combined:
+                header += "(combined_eye_origin.x,combined_eye_origin.y,combined_eye_origin.z)" + delimiter + "(combined_eye_gaze.x,combined_eye_gaze.y,combined_eye_gaze.z)" + delimiter;
+                break;
+            case EyeTrackingOptions.all:
+                header += "(left_eye_origin.x,left_eye_origin.y,left_eye_origin.z)" + delimiter + "(left_eye_gaze.x,left_eye_gaze.y,left_eye_gaze.z)" + delimiter;
+                header += "(right_eye_origin.x,right_eye_origin.y,right_eye_origin.z)" + delimiter + "(right_eye_gaze.x,right_eye_gaze.y,right_eye_gaze.z)" + delimiter;
+                header += "(combined_eye_origin.x,combined_eye_origin.y,combined_eye_origin.z)" + delimiter + "(combined_eye_gaze.x,combined_eye_gaze.y,combined_eye_gaze.z)" + delimiter;
+                break;
+        }
+        if(savePosition)
+            header += "(" + trackedGameobject.name + "_position.x" + ",";
+            header += trackedGameobject.name + "_position.y" + ",";
+            header += trackedGameobject.name + "_position.z" + ")" + delimiter;
+        if(saveOrientation)
+            header += "(" + trackedGameobject.name + "_rotation.w" + ",";
+            header += trackedGameobject.name + "_rotation.x" + ",";
+            header += trackedGameobject.name + "_rotation.y" + ",";
+            header += trackedGameobject.name + "_rotation.z" + ")" + delimiter;
+        header += "messages" + delimiter;
+        sw.WriteLine(header);
+        sw.Close();
+    }
 }
